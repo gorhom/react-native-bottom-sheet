c463c7818a7b5610c884ba2f5dc8c05f76a470ba
Mo Gorhom <gorhom@me.com>
2020-08-10 20:37:58 +0200

    chore: updated dependencies

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,26 +1,29 @@
 const path = require('path');
+const fs = require('fs');
 const blacklist = require('metro-config/src/defaults/blacklist');
 const escape = require('escape-string-regexp');
-const pak = require('../package.json');
 
 const root = path.resolve(__dirname, '..');
+const pak = JSON.parse(
+  fs.readFileSync(path.join(root, 'package.json'), 'utf8')
+);
 
-const modules = Object.keys({
-  ...pak.peerDependencies,
-});
+const modules = [
+  '@babel/runtime',
+  ...Object.keys({
+    ...pak.dependencies,
+    ...pak.peerDependencies,
+  }),
+];
 
 module.exports = {
   projectRoot: __dirname,
   watchFolders: [root],
 
-  // We need to make sure that only one version is loaded for peerDependencies
-  // So we blacklist them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: blacklist(
-      modules.map(
-        m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      )
-    ),
+    blacklistRE: blacklist([
+      new RegExp(`^${escape(path.join(root, 'node_modules'))}\\/.*$`),
+    ]),
 
     extraNodeModules: modules.reduce((acc, name) => {
       acc[name] = path.join(__dirname, 'node_modules', name);
