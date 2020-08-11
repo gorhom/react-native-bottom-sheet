9d5830ff264d31f98b90aa54d707a8b0f19ac5bd
Mo Gorhom <gorhom@me.com>
2020-08-11 20:35:24 +0200

    chore: updated android icon app

--- example/android/app/src/main/java/dev/gorhom/bottomsheet/MainActivity.java
+++ example/android/app/src/main/java/dev/gorhom/bottomsheet/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.reactnativebottomsheet;
+package dev.gorhom.bottomsheet;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- example/android/app/src/main/java/dev/gorhom/bottomsheet/MainApplication.java
+++ example/android/app/src/main/java/dev/gorhom/bottomsheet/MainApplication.java
@@ -1,4 +1,4 @@
-package com.example.reactnativebottomsheet;
+package dev.gorhom.bottomsheet;
 
 import android.app.Application;
 import android.content.Context;
@@ -59,7 +59,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.reactnativebottomsheetExample.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("dev.gorhom.bottomsheet.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
