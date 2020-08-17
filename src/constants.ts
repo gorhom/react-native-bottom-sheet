dada4f34439fe348a89a8aa0551213ee76689ecc
sa8ab <59901734+sa8ab@users.noreply.github.com>
2020-08-17 13:20:33 +0400

    Update Easing to work with reanimated 2

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-import Animated, { Easing } from 'react-native-reanimated';
+import Animated, { EasingNode } from 'react-native-reanimated';
 
-export const DEFAULT_ANIMATION_EASING: Animated.EasingFunction = Easing.out(
-  Easing.back(0.75)
+export const DEFAULT_ANIMATION_EASING: Animated.EasingFunction = EasingNode.out(
+  EasingNode.back(0.75)
 );
 export const DEFAULT_ANIMATION_DURATION = 500;
