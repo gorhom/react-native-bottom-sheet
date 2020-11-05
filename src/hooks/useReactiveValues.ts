db0ac3c694f16a3f1cc883b6b1b187a83399e3e7
Mo Gorhom <gorhom@me.com>
2020-11-05 23:43:07 +0100

    fix: updated useReactiveValues

--- src/hooks/useReactiveValues.ts
+++ src/hooks/useReactiveValues.ts
@@ -28,6 +28,19 @@ export const useReactiveValues = (values: number[]) => {
           ref.current.push(new Animated.Value(value));
         }
       });
+
+      /**
+       * if previous animated array has more values than the updated
+       * array, we will need to set the extra values to the last
+       * value of the updated array.
+       */
+      if (values.length < ref.current.length) {
+        const lastValue = values[values.length - 1];
+        for (let i = values.length - 1; i <= ref.current.length - 1; i++) {
+          ref.current[i].setValue(lastValue);
+        }
+      }
+      console.log('values', values, 'current length', ref.current.length);
     }
   }, [values]);
 
