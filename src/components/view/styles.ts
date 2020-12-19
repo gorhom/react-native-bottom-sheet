69ec6425ad0711e859609efea3dc9f07aa9d5f4c
Mo Gorhom <gorhom@me.com>
2020-12-19 22:15:23 +0100

    chore: updated pre-integrated view

--- src/components/view/styles.ts
+++ src/components/view/styles.ts
@@ -2,6 +2,6 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
   },
 });
--- src/components/view/types.d.ts
+++ src/components/view/types.d.ts
@@ -1,7 +1,7 @@
 import type { EffectCallback, DependencyList } from 'react';
 import type { ViewProps as RNViewProps } from 'react-native';
 
-export type BottomSheetViewProps = RNViewProps & {
+export interface BottomSheetViewProps extends RNViewProps {
   children: React.ReactNode[] | React.ReactNode;
   focusHook?: (effect: EffectCallback, deps?: DependencyList) => void;
-};
+}
