6223a4c8272fbc28a2f09d8cdf94f490e39c5e3a
Mo Gorhom <gorhom@me.com>
2021-01-09 15:53:40 +0100

    fix: exp easing flickers on android (#175)
    * chore: added a modified exp function
    
    * chore: updated useBottomSheetTimingConfigs to includes default values

---
+++ src/utilities/easingExp.ts
@@ -0,0 +1,10 @@
+/**
+ * A modified version of the default AnimatedEasing.exp,
+ * to insure its value never goes below `0`.
+ * @see https://github.com/software-mansion/react-native-reanimated/issues/1610
+ * @param t number
+ */
+export const exp = (t: number) => {
+  'worklet';
+  return Math.min(Math.max(0, Math.pow(2, 10 * (t - 1))), 1);
+};
