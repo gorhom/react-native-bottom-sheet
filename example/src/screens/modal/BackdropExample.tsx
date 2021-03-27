6589d764e8bdd4907b480c684cdefc12b27cacfd
Mo Gorhom <gorhom@me.com>
2021-03-27 21:12:57 +0000

    chore: updated backdrop examples

--- example/src/screens/modal/BackdropExample.tsx
+++ example/src/screens/modal/BackdropExample.tsx
@@ -1,22 +1,63 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 import { View, StyleSheet, Alert } from 'react-native';
-import { BottomSheetModal, BottomSheetBackdrop } from '@gorhom/bottom-sheet';
+import {
+  BottomSheetModal,
+  BottomSheetBackdrop,
+  BottomSheetBackdropProps,
+} from '@gorhom/bottom-sheet';
 import Button from '../../components/button';
 import ContactListContainer from '../../components/contactListContainer';
 import withModalProvider from '../withModalProvider';
 
 const BackdropExample = () => {
+  // state
+  const [backdropPressBehavior, setBackdropPressBehavior] = useState<
+    'none' | 'close' | 'collapse'
+  >('collapse');
+
+  // refs
   const bottomSheetRef = useRef<BottomSheetModal>(null);
 
-  // callbacks
+  // variables
+  const snapPoints = useMemo(() => ['25%', '50%'], []);
+
+  //#region callbacks
   const handleDismiss = useCallback(() => {
     Alert.alert('Modal Been Dismissed');
   }, []);
   const handlePresentPress = useCallback(() => {
     bottomSheetRef.current?.present();
   }, []);
+  const handleTogglePressBehavior = useCallback(() => {
+    setBackdropPressBehavior(state => {
+      switch (state) {
+        case 'none':
+          return 'close';
+        case 'close':
+          return 'collapse';
+        case 'collapse':
+          return 'none';
+      }
+    });
+  }, []);
+  const handleExpandPress = useCallback(() => {
+    bottomSheetRef.current?.expand();
+  }, []);
+  const handleCollapsePress = useCallback(() => {
+    bottomSheetRef.current?.collapse();
+  }, []);
+  const handleClosePress = useCallback(() => {
+    bottomSheetRef.current?.close();
+  }, []);
+  //#endregion
 
   // renders
+  const renderBackdrop = useCallback(
+    (props: BottomSheetBackdropProps) => (
+      <BottomSheetBackdrop {...props} pressBehavior={backdropPressBehavior} />
+    ),
+    [backdropPressBehavior]
+  );
   return (
     <View style={styles.container}>
       <Button
@@ -24,12 +65,31 @@ const BackdropExample = () => {
         style={styles.buttonContainer}
         onPress={handlePresentPress}
       />
+      <Button
+        label={`Toggle Press Behavior: ${backdropPressBehavior}`}
+        style={styles.buttonContainer}
+        onPress={handleTogglePressBehavior}
+      />
+      <Button
+        label="Expand"
+        style={styles.buttonContainer}
+        onPress={handleExpandPress}
+      />
+      <Button
+        label="Collapse"
+        style={styles.buttonContainer}
+        onPress={handleCollapsePress}
+      />
+      <Button
+        label="Close"
+        style={styles.buttonContainer}
+        onPress={handleClosePress}
+      />
       <BottomSheetModal
         ref={bottomSheetRef}
-        snapPoints={['25%', '50%']}
-        animationDuration={250}
+        snapPoints={snapPoints}
         onDismiss={handleDismiss}
-        backdropComponent={BottomSheetBackdrop}
+        backdropComponent={renderBackdrop}
       >
         <ContactListContainer title="Modal FlatList" type="FlatList" />
       </BottomSheetModal>
