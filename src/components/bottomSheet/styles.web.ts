293805c97332fcf5123829b4eaf0d319d142cc92
Jérémy Jourdin <jjourdin18@gmail.com>
2021-09-21 10:11:36 +0200

    Make bottom sheet fixed on web

---
+++ src/components/bottomSheet/styles.web.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column-reverse',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+  contentContainer: {},
+  contentMaskContainer: {
+    overflow: 'hidden',
+  },
+});
