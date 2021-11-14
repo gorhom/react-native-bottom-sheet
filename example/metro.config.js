4a4b6eb8cf41cb853d889039db445a0c3a93167b
AlbertGao <albertgaohy@gmail.com>
2021-11-15 00:36:15 +1300

    fix: error Cannot find module 'metro-config/src/defaults/blacklist' in example
    error Cannot find module 'metro-config/src/defaults/blacklist'
    Require stack:
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/metro.config.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/import-fresh/index.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/cosmiconfig/dist/loaders.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/cosmiconfig/dist/createExplorer.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/cosmiconfig/dist/index.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/@react-native-community/cli/build/tools/config/readConfigFromDisk.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/@react-native-community/cli/build/tools/config/index.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/@react-native-community/cli/build/commands/install/install.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/@react-native-community/cli/build/commands/index.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/@react-native-community/cli/build/index.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/react-native/cli.js.
    Error: Cannot find module 'metro-config/src/defaults/blacklist'
    Require stack:
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/metro.config.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/import-fresh/index.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/cosmiconfig/dist/loaders.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/cosmiconfig/dist/createExplorer.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/cosmiconfig/dist/index.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/@react-native-community/cli/build/tools/config/readConfigFromDisk.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/@react-native-community/cli/build/tools/config/index.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/@react-native-community/cli/build/commands/install/install.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/@react-native-community/cli/build/commands/index.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/@react-native-community/cli/build/index.js
    - /Users/albertgao/codes/temp/react-native-bottom-sheet/example/node_modules/react-native/cli.js
        at Function.Module._resolveFilename (node:internal/modules/cjs/loader:933:15)
        at Function.Module._load (node:internal/modules/cjs/loader:778:27)
        at Module.require (node:internal/modules/cjs/loader:1005:19)
        at require (node:internal/modules/cjs/helpers:102:18)
        at Object.<anonymous> (/Users/albertgao/codes/temp/react-native-bottom-sheet/example/metro.config.js:3:19)
        at Module._compile (node:internal/modules/cjs/loader:1101:14)
        at Object.Module._extensions..js (node:internal/modules/cjs/loader:1153:10)
        at Module.load (node:internal/modules/cjs/loader:981:32)
        at Function.Module._load (node:internal/modules/cjs/loader:822:12)
        at Module.require (node:internal/modules/cjs/loader:1005:19)
    info Run CLI with --verbose flag for more details.

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const blacklist = require('metro-config/src/defaults/blacklist');
+const blacklist = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
 
 const root = path.resolve(__dirname, '..');
