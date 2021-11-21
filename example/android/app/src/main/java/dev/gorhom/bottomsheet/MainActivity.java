9176e35dec148a8d3eff8b472ccb495b4992d8e1
gorhom <gorhom.dev@gmail.com>
2021-11-21 12:52:27 +0000

    chore: updated example dependencies

--- example/android/app/src/main/java/dev/gorhom/bottomsheet/MainActivity.java
+++ example/android/app/src/main/java/dev/gorhom/bottomsheet/MainActivity.java
@@ -1,11 +1,18 @@
 package dev.gorhom.bottomsheet;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
