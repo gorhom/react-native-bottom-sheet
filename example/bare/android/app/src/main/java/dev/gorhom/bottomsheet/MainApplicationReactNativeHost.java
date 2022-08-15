cebae97c56f0b2ff31c247b1fce5cbe8172b6554
Mo Gorhom <gorhom.dev@gmail.com>
2022-08-15 16:06:40 +0100

    chore: updated example packages (#1064)

--- example/bare/android/app/src/main/java/dev/gorhom/bottomsheet/MainApplicationReactNativeHost.java
+++ example/bare/android/app/src/main/java/dev/gorhom/bottomsheet/MainApplicationReactNativeHost.java
@@ -18,7 +18,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.UIManager;
 import com.facebook.react.fabric.ComponentFactory;
 import com.facebook.react.fabric.CoreComponentsRegistry;
-import com.facebook.react.fabric.EmptyReactNativeConfig;
+import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.uimanager.ViewManagerRegistry;
 
@@ -108,7 +108,7 @@ public class MainApplicationReactNativeHost extends ReactNativeHost {
               return new FabricJSIModuleProvider(
                 reactApplicationContext,
                 componentFactory,
-                new EmptyReactNativeConfig(),
+                ReactNativeConfig.DEFAULT_CONFIG,
                 viewManagerRegistry);
             }
           });
