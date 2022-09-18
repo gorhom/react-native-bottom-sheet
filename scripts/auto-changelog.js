ee6230c7e75c03fec1887fe84bc2a0e01f0b1c62
gorhom <gorhom.dev@gmail.com>
2022-09-18 21:59:12 +0100

    chore: updated changelog script and templates

--- scripts/auto-changelog.js
+++ scripts/auto-changelog.js
@@ -1,37 +1,44 @@
 module.exports = function (Handlebars) {
-  Handlebars.registerHelper('custom', function (context, extra, options) {
-    if ((!context || context.length === 0) && (!extra || extra.length === 0)) {
-      return '';
-    }
+  Handlebars.registerHelper(
+    'custom',
+    function (context, merges, fixes, options) {
+      if (
+        (!context || context.length === 0) &&
+        (!merges || merges.length === 0) &&
+        (!fixes || fixes.length === 0)
+      ) {
+        return '';
+      }
 
-    const list = [...context, ...extra]
-      .filter(item => {
-        const commit = item.commit || item;
-        if (options.hash.exclude) {
-          const pattern = new RegExp(options.hash.exclude, 'm');
-          if (pattern.test(commit.message)) {
-            return false;
+      const list = [...context, ...(merges || []), ...(fixes || [])]
+        .filter(item => {
+          const commit = item.commit || item;
+          if (options.hash.exclude) {
+            const pattern = new RegExp(options.hash.exclude, 'm');
+            if (pattern.test(commit.message)) {
+              return false;
+            }
           }
-        }
-        if (options.hash.message) {
-          const pattern = new RegExp(options.hash.message, 'm');
-          return pattern.test(commit.message);
-        }
-        if (options.hash.subject) {
-          const pattern = new RegExp(options.hash.subject);
-          return pattern.test(commit.subject);
-        }
-        return true;
-      })
-      .map(item => options.fn(item))
-      .join('');
+          if (options.hash.message) {
+            const pattern = new RegExp(options.hash.message, 'm');
+            return pattern.test(commit.message);
+          }
+          if (options.hash.subject) {
+            const pattern = new RegExp(options.hash.subject);
+            return pattern.test(commit.subject);
+          }
+          return true;
+        })
+        .map(item => options.fn(item.commit || item))
+        .join('');
 
-    if (!list) {
-      return '';
+      if (!list) {
+        return '';
+      }
+
+      return options.hash.heading
+        ? `${options.hash.heading}\n\n${list}`
+        : `${list}`;
     }
-
-    return options.hash.heading
-      ? `${options.hash.heading}\n\n${list}`
-      : `${list}`;
-  });
+  );
 };
