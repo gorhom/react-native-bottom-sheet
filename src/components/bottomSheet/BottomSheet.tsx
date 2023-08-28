55d7857932bb42d3046148eeb955ea8bf2542488
Emilio Schepis <emilio.schepis@gmail.com>
2023-08-28 10:53:02 +0000

    fix: non-zero duration for resize animation
    Using Reanimated 3, this resize animation with duration 0
    causes the bottom sheet modal to disappear.
    
    Refs: #1356

--- src/components/bottomSheet/BottomSheet.tsx
+++ src/components/bottomSheet/BottomSheet.tsx
@@ -1360,7 +1360,7 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
           if (containerHeight !== _previousContainerHeight) {
             animationSource = ANIMATION_SOURCE.CONTAINER_RESIZE;
             animationConfig = {
-              duration: 0,
+              duration: 1,
             };
           }
         }
