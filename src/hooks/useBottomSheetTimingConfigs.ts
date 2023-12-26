f1c70e740badd64244dafc513625fd88091166a9
gorhom <gorhom.dev@gmail.com>
2023-12-26 19:21:29 +0100

    fix: fixed position x index sequencing with container resizing

--- src/hooks/useBottomSheetTimingConfigs.ts
+++ src/hooks/useBottomSheetTimingConfigs.ts
@@ -1,14 +1,10 @@
 import { useMemo } from 'react';
 import type { EasingFunction } from 'react-native';
-import type {
-  EasingFunctionFactory,
-  ReduceMotion,
-} from 'react-native-reanimated';
+import type { EasingFunctionFactory } from 'react-native-reanimated';
 import { ANIMATION_DURATION, ANIMATION_EASING } from '../constants';
 
 interface TimingConfig {
   duration?: number;
-  reduceMotion?: ReduceMotion;
   easing?: EasingFunction | EasingFunctionFactory;
 }
 
