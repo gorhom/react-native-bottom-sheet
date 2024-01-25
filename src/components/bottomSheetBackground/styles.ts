a4fd1666b73c34edf89cb4b504c465b2f619d36e
abhijithsajeev27 <115783517+abhijithsajeev27@users.noreply.github.com>
2024-01-25 11:16:36 +0530

    color change

--- src/components/bottomSheetBackground/styles.ts
+++ src/components/bottomSheetBackground/styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    backgroundColor: 'red',
     borderRadius: 15,
   },
 });
