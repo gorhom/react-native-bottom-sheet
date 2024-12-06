3845bb830d1afe0314826557bdbd3f2ca3454845
Wawrzyniec Wawro <wawrzyniec@wawro.eu>
2024-12-06 18:28:44 +0000

    preventing multiple calls for onClose
    currently `_providedOnClose` gets called potentially multiple times as this reanimated hook may (and likely will) run multiple times - I believe it should be called only once when changing `_animatedIndex` to `-1` only

--- src/components/bottomSheet/BottomSheet.tsx
+++ src/components/bottomSheet/BottomSheet.tsx
@@ -1836,7 +1836,7 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
         /**
          * if index is `-1` than we fire the `onClose` callback.
          */
-        if (_animatedIndex === -1 && _providedOnClose) {
+        if (_animatedIndex === -1 && _animatedIndex !== animatedCurrentIndex.value && _providedOnClose) {
           if (__DEV__) {
             runOnJS(print)({
               component: BottomSheet.name,
