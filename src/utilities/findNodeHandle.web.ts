6442b0ea54a38d8dcb82f63aade077ead29d382b
gorhom <gorhom.dev@gmail.com>
2025-05-26 21:43:59 +0200

    fix(#2237): fixed node handle lookup for virtualized list on web (by @btoo)

--- src/utilities/findNodeHandle.web.ts
+++ src/utilities/findNodeHandle.web.ts
@@ -1,19 +1,33 @@
-import type React from 'react';
-import { findNodeHandle as _findNodeHandle } from 'react-native';
+import {
+  type NodeHandle,
+  findNodeHandle as _findNodeHandle,
+} from 'react-native';
 
 export function findNodeHandle(
-  componentOrHandle:
-    | null
-    | number
-    // biome-ignore lint/suspicious/noExplicitAny: fix later
-    | React.Component<any, any>
-    // biome-ignore lint/suspicious/noExplicitAny: fix later
-    | React.ComponentClass<any>
+  componentOrHandle: Parameters<typeof _findNodeHandle>['0']
 ) {
+  let nodeHandle: NodeHandle | null;
   try {
-    return _findNodeHandle(componentOrHandle);
-  } catch {
+    nodeHandle = _findNodeHandle(componentOrHandle);
+    if (nodeHandle) {
+      return nodeHandle;
+    }
+  } catch {}
+
+  try {
     // @ts-ignore
-    return componentOrHandle.getNativeScrollRef();
+    nodeHandle = componentOrHandle.getNativeScrollRef();
+    if (nodeHandle) {
+      return nodeHandle;
+    }
+  } catch {}
+
+  // @ts-ignore https://github.com/facebook/react-native/blob/a314e34d6ee875830d36e4df1789a897c7262056/packages/virtualized-lists/Lists/VirtualizedList.js#L1252
+  nodeHandle = componentOrHandle._scrollRef;
+  if (nodeHandle) {
+    return nodeHandle;
   }
+
+  console.warn('could not find scrollable ref!');
+  return componentOrHandle;
 }
