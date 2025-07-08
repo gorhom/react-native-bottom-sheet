b25066c0c4ba8e406c88c3ee03050b931a563d5f
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-07-08 16:01:47 +0200

    Migrate from `useWorkletCallback`

--- src/hooks/useKeyboard.ts
+++ src/hooks/useKeyboard.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import {
   Keyboard,
   type KeyboardEvent,
@@ -10,7 +10,6 @@ import {
   runOnUI,
   useAnimatedReaction,
   useSharedValue,
-  useWorkletCallback,
 } from 'react-native-reanimated';
 import { KEYBOARD_STATE, SCREEN_HEIGHT } from '../constants';
 
@@ -42,7 +41,7 @@ export const useKeyboard = () => {
   //#endregion
 
   //#region worklets
-  const handleKeyboardEvent = useWorkletCallback(
+  const handleKeyboardEvent = useCallback(
     (
       state: KEYBOARD_STATE,
       height: number,
@@ -50,6 +49,7 @@ export const useKeyboard = () => {
       easing: KeyboardEventEasing,
       bottomOffset?: number
     ) => {
+      'worklet';
       if (state === KEYBOARD_STATE.SHOWN && !shouldHandleKeyboardEvents.value) {
         /**
          * if the keyboard event was fired before the `onFocus` on TextInput,
