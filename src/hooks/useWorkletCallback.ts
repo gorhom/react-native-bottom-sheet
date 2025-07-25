a6dfe2c119f69cf72a0bbb7a09d8e03272e73b96
Lucas Alves <lucas.developing@gmail.com>
2025-07-25 10:16:46 -0300

    fix: add the useCallback approach

--- src/hooks/useWorkletCallback.ts
+++ src/hooks/useWorkletCallback.ts
@@ -1,8 +1,8 @@
-import { useCallback} from 'react';
+import { useCallback } from 'react';
 import * as Reanimated from 'react-native-reanimated';
 
 
-// @ts-ignore Runtime check if has useWorkletCallback for supporting reanimated v3 and v4 at the same time.
+// @ts-ignore runtime check if has useWorkletCallback for supporting reanimated v3 and v4 at the same time.
 const hasCallbackWorklet = typeof Reanimated?.useWorkletCallback === 'function';
 if(!hasCallbackWorklet){
     console.warn('You are using react-native-reanimated v3 please upgrade to v4 as soon as possible.');
@@ -14,10 +14,4 @@ if(!hasCallbackWorklet){
  */
 export const useWorkletCallback = hasCallbackWorklet
   ? (Reanimated.useWorkletCallback as <T extends (...args: any[]) => any>(cb: T, deps: readonly unknown[]) => T)
-  : function useWorkletCallback<T extends (...args: any[]) => any>(callback: T, deps: readonly unknown[]): T {
-      return useCallback((...args: Parameters<T>): ReturnType<T> => {
-        'worklet';
-        return callback?.(...args);
-      }, deps) as T;
-
-    };
+  : useCallback;
