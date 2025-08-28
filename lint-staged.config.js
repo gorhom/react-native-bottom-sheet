eb03ab074dc17df9c3ff404f645db3bfbc0c6aab
gorhom <gorhom.dev@gmail.com>
2025-08-28 22:10:56 +0200

    chore: updated biome dep

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,6 @@ module.exports = {
   '*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}': [
     'biome check --files-ignore-unknown=true', // Check formatting and lint
     'biome check --write --no-errors-on-unmatched', // Format, sort imports, lint, and apply safe fixes
-    'biome check --write --organize-imports-enabled=false --no-errors-on-unmatched', // format and apply safe fixes
     'biome check --write --unsafe --no-errors-on-unmatched', // Format, sort imports, lints, apply safe/unsafe fixes
     'biome format --write --no-errors-on-unmatched', // Format
     'biome lint --write --no-errors-on-unmatched', // Lint and apply safe fixes
