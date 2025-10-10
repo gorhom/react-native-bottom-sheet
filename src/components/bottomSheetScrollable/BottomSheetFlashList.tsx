516ae261716a85c4ef871a794a243e5b5de2ebd3
Linus Unnebäck <linus@folkdatorn.se>
2025-10-10 10:53:01 +0200

    Avoid deprecated AnimateProps type

--- src/components/bottomSheetScrollable/BottomSheetFlashList.tsx
+++ src/components/bottomSheetScrollable/BottomSheetFlashList.tsx
@@ -2,7 +2,7 @@
 import type { FlashListProps } from '@shopify/flash-list';
 import React, { forwardRef, memo, type Ref, useMemo } from 'react';
 import type { ScrollViewProps } from 'react-native';
-import type Animated from 'react-native-reanimated';
+import type { AnimatedProps } from 'react-native-reanimated';
 import BottomSheetScrollView from './BottomSheetScrollView';
 import type {
   BottomSheetScrollViewMethods,
@@ -19,7 +19,7 @@ try {
 } catch (_) {}
 
 export type BottomSheetFlashListProps<T> = Omit<
-  Animated.AnimateProps<FlashListProps<T>>,
+  AnimatedProps<FlashListProps<T>>,
   'decelerationRate' | 'onScroll' | 'scrollEventThrottle'
 > &
   BottomSheetScrollableProps & {
