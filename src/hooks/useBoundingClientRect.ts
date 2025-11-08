a2162002ef4395ba2e50e7eacfd1180c916cc863
Chima Nwachukwu <87417020+tylerdgenius@users.noreply.github.com>
2025-11-08 17:12:28 +0100

    fix: fixed function undefined error in useBoundingClientRect

--- src/hooks/useBoundingClientRect.ts
+++ src/hooks/useBoundingClientRect.ts
@@ -55,17 +55,20 @@ export function useBoundingClientRect(
       return;
     }
 
-    // @ts-ignore 👉 https://github.com/facebook/react/commit/53b1f69ba
-    if (ref.current.unstable_getBoundingClientRect !== null) {
-      // @ts-ignore https://github.com/facebook/react/commit/53b1f69ba
+    // @ts-expect-error 👉 https://github.com/facebook/react/commit/53b1f69ba
+    if (
+      ref.current.unstable_getBoundingClientRect !== null &&
+      typeof ref.current.unstable_getBoundingClientRect === 'function'
+    ) {
+      // @ts-expect-error https://github.com/facebook/react/commit/53b1f69ba
       const layout = ref.current.unstable_getBoundingClientRect();
       handler(layout);
       return;
     }
 
-    // @ts-ignore once it `unstable_getBoundingClientRect` gets stable 🤞.
+    // @ts-expect-error once it `unstable_getBoundingClientRect` gets stable 🤞.
     if (ref.current.getBoundingClientRect !== null) {
-      // @ts-ignore once it `unstable_getBoundingClientRect` gets stable.
+      // @ts-expect-error once it `unstable_getBoundingClientRect` gets stable.
       const layout = ref.current.getBoundingClientRect();
       handler(layout);
     }
